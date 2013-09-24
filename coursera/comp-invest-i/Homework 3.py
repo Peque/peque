8b07d4472f4105af76c9c0e0de7e81ae00add8aa
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2013-09-25 00:44:14 +0200

    Homework 3: getting closer

--- coursera/comp-invest-i/Homework 3.py
+++ coursera/comp-invest-i/Homework 3.py
@@ -7,41 +7,102 @@ import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.DataAccess as da
 
 #
+# Command line arguments (TODO)
+#
+orders_file = 'orders.csv'
+values_file = 'values.csv'
+initial_wealth = 1000000
+benchmark = '$SPX'
+
+#
 # Load orders from CSV file, and sort them by date (oldest first)
 #
 # We are supposing the columns are as follow:
 #
 #   [ Year, Month, Day, Equity, Order, Quantity ]
 #
-orders = pd.read_csv('orders.csv', delimiter=',', usecols = range(6), header = None)
-orders.sort([0, 1, 2], ascending = [1, 1, 1], inplace = True)
+def parse_date_function(year, month, day):
+	return dt.datetime(year, month, day).date()
+
+orders = pd.read_csv(orders_file, delimiter=',',
+                     usecols = range(6),
+                     names = ['year', 'month', 'day', 'equity', 'order', 'shares'],
+                     header = None,
+                     skipinitialspace = True,
+                     parse_dates = {'date' : ['year', 'month', 'day']},
+                     date_parser=parse_date_function)
+
+orders.sort(['date'], ascending = [1], inplace = True)
+orders.index = range(len(orders))
 
 #
 # Get the symbol list
 #
-symbols = orders[3].unique()
+symbols = orders['equity'].unique()
 
 #
 # Get first and last order date
 #
-startdate = dt.datetime(orders.head(1)[0], orders.head(1)[1], orders.head(1)[2])
-enddate = dt.datetime(orders.tail(1)[0], orders.tail(1)[1], orders.tail(1)[2])
+startdate = orders['date'][0]
+enddate = orders['date'][len(orders) - 1]
 
-print symbols
-print orders
-print startdate
-print enddate
-
+#
+# Get close data
+#
 ldt_timestamps = du.getNYSEdays(startdate, enddate, dt.timedelta(hours=16))
-
 c_dataobj = da.DataAccess('Yahoo')
 ldf_data = c_dataobj.get_data(ldt_timestamps, symbols, ['close'])
 d_data = dict(zip(['close'], ldf_data))
-
 close_data = d_data['close'].values
+close_dataframe = pd.DataFrame(close_data, range(len(close_data)), symbols)
+
+#
+# Iterate for investing
+#
+shares = {}
+wealth_history = [None] * len(ldt_timestamps)
+cash_wealth = initial_wealth
+for equity in symbols:
+	shares[equity] = 0
+j = 0
+for i in range(len(ldt_timestamps)):
+	while ldt_timestamps[i].date() == orders['date'][j]:
+		current_shares = orders['shares'][j]
+		current_equity = orders['equity'][j]
+		current_order =  orders['order'][j]
+		if current_order == 'Buy':
+			shares[current_equity] += current_shares
+			cash_wealth -= current_shares*close_dataframe[current_equity][i]
+		if current_order == 'Sell':
+			shares[current_equity] -= current_shares
+			cash_wealth += current_shares*close_dataframe[current_equity][i]
+		j += 1
+		if j == len(orders):
+			break
+	invested_wealth = 0
+	for equity in symbols:
+		invested_wealth += close_dataframe[equity][i]*shares[equity]
+	total_wealth = invested_wealth + cash_wealth
+	wealth_history[i] = total_wealth
+	i += 1
+
+ldf_data = c_dataobj.get_data(ldt_timestamps, [benchmark], ['close'])
+d_data = dict(zip(['close'], ldf_data))
+close_data = d_data['close'].values
+close_data *= 1000000/close_data[0]
+
+print "Data Range : " + str(startdate) + " to " + str(enddate)
+print "Using " + benchmark + " as benchmark..."
+print "Total return (portfolio): " + str(wealth_history[len(wealth_history) - 1] / initial_wealth)
+print "Total return (benchmark): " + str(close_data[len(close_data) - 1] / initial_wealth)
+
+#
+# Plot benchmark
+#
 plt.clf()
+plt.plot(ldt_timestamps, wealth_history)
 plt.plot(ldt_timestamps, close_data)
-plt.legend(symbols)
+plt.legend(['Portfolio', '$SPX'])
 plt.ylabel('Adjusted Close')
 plt.xlabel('Date')
 plt.savefig('Homework 3 - adjustedclose.pdf', format='pdf')
